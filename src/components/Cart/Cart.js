c4d51e33837dc51aed4bad52325c62ca9010e3db
pramod ade <prmdextracthere@gmail.com>
2024-03-02 15:29:32 +0530

    Making a hard Coded Cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import classes from './Cart.module.css'
+import Modal from '../UI/Modal';
+
+const Cart = (props) => {
+  const cartItem = <ul className={classes.cartItems}> {[{id:'c1', name:"shushi" , amount:2 , price: 12.99}].map((item)=>(<li>{item.name}</li>))} </ul>;
+
+
+  return (
+    <Modal>
+      {cartItem}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button-alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  )
+}
+
+export default Cart
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import classes from './Input.module.css'
+
 const Input = (props) => {
   return (
     <div className={classes.input}>
